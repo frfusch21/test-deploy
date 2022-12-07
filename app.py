86df316b316e6f6a6fd48eb3a85d970b3ed4760e
frfusch21 <reviewfarmei20@gmail.com>
2022-12-07 21:04:35 +0700

    first deploy

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, make_response
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/predict", methods=['GET','POST'])
+def pred():
+    df = pd.DataFrame(columns=['age','sex','bmi','children','smoker','region'])
+
+    if request.method == "POST":
+        name = request.form["nama"]
+        age = request.form["age"]
+        sex = request.form["sex"]
+        bmi = request.form["bmi"]
+        children = request.form["children"]
+        smoker = request.form["smoker"]
+        region = request.form["region"]
+
+        df = df.append({"age":age, "sex":sex, "bmi":bmi, "children":children, 
+        "smoker":smoker,"region":region}, ignore_index=True)
+        with open("model/Lin_reg_model.pkl","rb") as file:
+            Lin_reg_model = pickle.load(file)
+        
+        prediction = Lin_reg_model.predict(df)
+
+        return render_template("result.html", pred=str(prediction[0]), name=name)
+
+if __name__ == "__main__":
+  app.run()
